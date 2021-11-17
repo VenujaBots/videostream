4a514badc3497b61e660d956b47b5f2e84f20994
Venuja Sadew <91406478+VenujaBots@users.noreply.github.com>
2021-11-17 18:38:29 +0530

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, idle
 import os
 from Bot.video_stream import app
-API_ID = os.environ.get("API_ID",12345)
-API_HASH = os.environ.get("API_HASH","")
-TOKEN = os.environ.get("TOKEN","")
+API_ID = os.environ.get("API_ID",18862638)
+API_HASH = os.environ.get("API_HASH","2a4a8dc0c1f6c9cb65f9f144439558ae")
+TOKEN = os.environ.get("TOKEN","2143873439:AAF4v4I1m6y38KpkDFZpmSLKPHZW_0UKHa8")
 
 bot = Client(
     ":memory:",
